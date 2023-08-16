9d73b91b984f2ea1ee3ed6aaa4721dcdc52aa1c0
Andrej Jegorov <Jesource@users.noreply.github.com>
2023-08-16 12:49:34 +0300

    Changed method return types + calling them from Main.java

--- ClientCastleConquerors/client-lib/src/main/java/main/ClientNetwork.java
+++ ClientCastleConquerors/client-lib/src/main/java/main/ClientNetwork.java
@@ -1,33 +1,49 @@
 package main;
 
+import exceptions.ClientNetworkException;
+import messagesbase.ResponseEnvelope;
+import messagesbase.UniqueGameIdentifier;
+import messagesbase.UniquePlayerIdentifier;
+import messagesbase.messagesfromclient.PlayerRegistration;
+import messagesbase.messagesfromserver.ERequestState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import exceptions.ClientNetworkException;
-import messagesbase.ResponseEnvelope;
-import messagesbase.UniquePlayerIdentifier;
-import messagesbase.messagesfromserver.ERequestState;
-import messagesbase.messagesfromclient.PlayerRegistration;
 import reactor.core.publisher.Mono;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ClientNetwork {
+    private static final Logger logger = LoggerFactory.getLogger(ClientNetwork.class);
     private WebClient baseWebClient;
     private String gameID;
-    private static final Logger logger = LoggerFactory.getLogger(ClientNetwork.class);
 
-    public ClientNetwork(String serverBaseUrl, String gameID) {
+    public ClientNetwork(String serverBaseUrl) {
         this.baseWebClient = WebClient.builder().baseUrl(serverBaseUrl + "/games")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE)
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_XML_VALUE).build();
-        this.gameID = gameID;
     }
 
-    public UniquePlayerIdentifier sendPlayerRegistration(PlayerRegistration playerReg) throws ClientNetworkException {
+    public String retrieveUniqueGameIdentifier() throws ClientNetworkException {
+        try {
+            UniqueGameIdentifier uniqueGameIdentifier = baseWebClient.get()
+                    .uri("/")
+                    .retrieve()
+                    .bodyToMono(UniqueGameIdentifier.class)
+                    .block();
+            this.gameID = uniqueGameIdentifier.getUniqueGameID();
+            return this.gameID;
+        } catch (Exception e) {
+            logger.error("Error while fetching UniqueGameIdentifier", e);
+            throw new ClientNetworkException("Error retrieving game identifier");
+        }
+
+//        return
+    }
+
+    public String sendPlayerRegistration(PlayerRegistration playerReg) throws ClientNetworkException {
         Mono<ResponseEnvelope> webAccess = baseWebClient.method(HttpMethod.POST).uri("/" + this.gameID + "/players")
                 .body(BodyInserters.fromValue(playerReg)) // specify the data which is sent to the server
                 .retrieve().bodyToMono(ResponseEnvelope.class); // specify the object returned by the server
@@ -35,9 +51,11 @@ public class ClientNetwork {
         if (resultReg.getState() == ERequestState.Error) {
             System.err.println("Client error, errormessage: " + resultReg.getExceptionMessage());
         } else {
-            return resultReg.getData().get();
+            return resultReg.getData().get().getUniquePlayerID();
         }
         logger.error("Registration error!");
         throw new ClientNetworkException("Registration error!");
     }
+
+
 }
--- ClientCastleConquerors/client-lib/src/main/java/main/Main.java
+++ ClientCastleConquerors/client-lib/src/main/java/main/Main.java
@@ -1,5 +1,20 @@
 package main;
 
-public class Main {
+import messagesbase.messagesfromclient.PlayerRegistration;
 
+public class Main {
+    public static void main(String[] args) {
+        ClientNetwork clientNetwork = new ClientNetwork("http://localhost:18235");
+        String gameID = "", playerID = "";
+
+        try {
+             gameID = clientNetwork.retrieveUniqueGameIdentifier();
+             playerID = clientNetwork.sendPlayerRegistration(
+                    new PlayerRegistration("test_username")
+            );
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+        System.out.println(gameID +" " + playerID);
+    }
 }
