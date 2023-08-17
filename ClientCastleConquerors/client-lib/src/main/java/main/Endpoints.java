8f8185a5c51202626413f191a8f9ff714eb32a44
Vladimir Bulgakov <a12132881@unet.univie.ac.at>
2023-08-17 13:17:49 +0300

    Add controller to Client

---
+++ ClientCastleConquerors/client-lib/src/main/java/main/Endpoints.java
@@ -0,0 +1,40 @@
+package main;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class Endpoints {
+	
+	@GetMapping("/")
+    public String getHomePage() {
+		System.out.println("I AM HERE");
+        return "homePage";
+    }
+	
+	@GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+	
+	@GetMapping("/game") 
+	public String getGamePage(Model model, HttpSession session) {
+		if (!isLoggedIn(session)) return "redirect:/login";
+    	String loggedInUser = getLoggedInUser(session);
+        model.addAttribute("loggedInUser", loggedInUser);
+		return "gamePage";
+	}
+	
+	private boolean isLoggedIn(HttpSession session) {
+        return getLoggedInUser(session) != null;
+    }
+	
+	private String getLoggedInUser(HttpSession session) {
+        return (String) session.getAttribute("loggedInUser");
+    }
+}
