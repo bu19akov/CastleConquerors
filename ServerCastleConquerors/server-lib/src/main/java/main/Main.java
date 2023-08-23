88315ba2e6cc7aa7c5d15ba114f338d646680f6e
Vladimir Bulgakov <a12132881@unet.univie.ac.at>
2023-08-23 19:39:33 +0300

    deployed server on heroku

--- ServerCastleConquerors/server-lib/src/main/java/main/Main.java
+++ ServerCastleConquerors/server-lib/src/main/java/main/Main.java
@@ -17,10 +17,6 @@ public class Main {
 	private static final int DEFAULT_PORT = 18235;
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Main.class);
-
-		// sets the port programmatically. One could also set it based on the .properties file
-		app.setDefaultProperties(Collections.singletonMap("server.port", DEFAULT_PORT));
-		app.run(args);
+		SpringApplication.run(Main.class, args);
 	}
 }
